c1732bc15696c61a2c441dc6b851f38d2ea8a4c1
Roman Chumak <chumakrom@gmail.com>
2024-12-19 13:21:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Breeze Mageants Estimated Delivery Date
+
+## Installation
+
+```bash
+composer require swissup/module-breeze-mageants-estimateddeliverydate
+bin/magento setup:upgrade --safe-mode=1
+```
---
+++ view/frontend/web/js/script.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    // $.widget('widgetName', {
+    //     component: 'Swissup_BreezeMageantsEstimateddeliverydate/js/component',
+    //     create: function () {
+    //         console.log(this.element);
+    //         console.log(this.options);
+    //     }
+    // });
+
+    $(document).on('breeze:load', () => {
+        console.log('hello from view/frontend/web/js/script.js');
+    });
+})();
